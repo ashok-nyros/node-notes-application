02132e3539b82b2128dd7113011504b7b3f38c8c
nyros <ashok.nyros@gmail.com>
2021-01-06 16:48:16 +0530

    limiting text fields

--- public/js/client.js
+++ public/js/client.js
@@ -6,32 +6,46 @@ window.onload = function() {
 const newbtn = document.getElementById("new");
 newbtn.addEventListener("click", openNote);
 const noteTitle = document.getElementById("notetitle");
+noteTitle.addEventListener("input", limitTitle);
 const noteDescription = document.getElementById("notedescription");
+noteDescription.addEventListener("input", limitBody)
 const saveNotebtn = document.getElementById("saveNote");
 saveNotebtn.addEventListener("click", saveNote)
 const deleteNotebtn = document.getElementById("deleteNote");
 deleteNotebtn.addEventListener("click", onDeleteNote);
 let accordion = document.getElementById("accordion");
 accordion.addEventListener("click", deleteNote);
+const titlelimit = document.getElementById("title-limit");
+const bodylimit = document.getElementById("body-limit");
+const notexterror = document.getElementById("no-text-error");
 var removeNoteTitle = "";
+
 //To open note modal
 function openNote() {
     console.log('clicked')
+    notetitle.value = "";
+    notedescription.value = "";
+    titlelimit.innerText = "";
+    bodylimit.innerText = "";
+    notexterror.innerText = "";
     $('#exampleModal').modal('show');
 }
 //To save new Note
 function saveNote(e) {
     e.preventDefault();
     if (!noteTitle.value) {
-        alert("enter note title");
+        // alert("enter note title");
+        notexterror.innerText = "Enter Note title to continue"
         notetitle.focus();
         return false;
     } else if (!noteDescription.value) {
-        alert("enter some note description please");
+        // alert("enter some note description please");
+        notexterror.innerText = "Enter Note description to continue"
         notedescription.focus();
         return false;
     } else {
-        let id = noteTitle.value.split(" ").join("")
+        // let id = noteTitle.value.split(" ").join("");
+        let id = noteTitle.value.replace(/\W/g, '_');
         fetch(`/addnote?title=${noteTitle.value}&body=${noteDescription.value}&noteID=${id}`).then(
             (res) => {
                 console.log(res);
@@ -60,14 +74,14 @@ function createList(data) {
     data.forEach(note => {
         noteContent += '<div class="card" id="card">' +
             '<div class="card-header">' +
-            '<a class="card-link" data-toggle="collapse" data-parent="#accordion" href="#collapse_' + note.id + '">' +
+            '<a class="card-link" data-toggle="collapse"  href="#collapse_' + note.id + '">' +
             note.title +
             '</a>' +
             '<div class="btns" style="float: right;">' +
             '<i class="fa fa-trash" style="cursor: pointer;"></i>' +
             '</div>' +
             '</div>' +
-            '<div id="collapse_' + note.id + '" class="collapse">' +
+            '<div id="collapse_' + note.id + '" class="collapse" data-parent="#accordion">' +
             '<div class="card-body">' +
             note.body +
             '</div>' +
@@ -90,17 +104,23 @@ function onDeleteNote() {
     fetch(`/removenote?title=${removeNoteTitle}`).then((res) => {
         console.log(typeof(res))
         res.json().then((data) => {
-            showNotification("Notes Deleting update", "Notes Deleted Successfully!")
             console.log(data);
-            if (data.length > 0) {
-                createList(data);
-            }
-            if (data.length === 0) {
-                console.log("it is empty");
-                accordion.innerHTML =
-                    ` <div class="no-notes">
+            if (data.error) {
+                console.log("error is" + data.error);
+                alert(data.error);
+                return false;
+            } else {
+                showNotification("Notes Deleting update", "Notes Deleted Successfully!")
+                if (data.length > 0) {
+                    createList(data);
+                }
+                if (data.length === 0) {
+                    console.log("it is empty");
+                    accordion.innerHTML =
+                        ` <div class="no-notes">
                             <p>No Notes Available.</p>
                       </div>`
+                }
             }
 
         });
@@ -119,8 +139,26 @@ function showNotification(message, type) {
     toast.innerHTML = toastContent;
     $('.toast').toast({
         animation: false,
-        delay: 5000
+        delay: 1000
     });
     $('.toast').toast('show');
 
+}
+
+function limitTitle(e) {
+    bodylimit.innerText = "";
+    let maxCharacters = 25;
+    console.log(e.target.value)
+    if (e.target.value.length <= maxCharacters) {
+        titlelimit.innerText = (maxCharacters - (e.target.value.length)) + " characters left"
+    }
+}
+
+function limitBody(e) {
+    titlelimit.innerText = "";
+    let maxCharacters = 250;
+    console.log(e.target.value)
+    if (e.target.value.length <= maxCharacters) {
+        bodylimit.innerText = (maxCharacters - (e.target.value.length)) + " characters left"
+    }
 }
